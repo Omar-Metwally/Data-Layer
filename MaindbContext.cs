2e72c0cbb0db37369a32587f985dadb3a94ea990
omar met <xomar.metwallyx@gmail.com>
2023-08-07 02:38:33 +0300

    v.071

--- MaindbContext.cs
+++ MaindbContext.cs
@@ -22,6 +22,10 @@ public partial class MaindbContext : DbContext
 
     public virtual DbSet<Customer> Customers { get; set; }
 
+    public virtual DbSet<District> Districts { get; set; }
+
+    public virtual DbSet<Governorate> Governorates { get; set; }
+
     public virtual DbSet<MeasuresOfScale> MeasuresOfScales { get; set; }
 
     public virtual DbSet<Order> Orders { get; set; }
@@ -32,20 +36,25 @@ public partial class MaindbContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
+    public virtual DbSet<Street> Streets { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseMySql("name=MainDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
-            .UseCollation("utf8mb3_general_ci")
-            .HasCharSet("utf8mb3");
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
 
         modelBuilder.Entity<Brand>(entity =>
         {
             entity.HasKey(e => e.BrandId).HasName("PRIMARY");
 
-            entity.ToTable("brands");
+            entity
+                .ToTable("brands")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.Property(e => e.BrandId)
                 .ValueGeneratedNever()
@@ -61,7 +70,10 @@ public partial class MaindbContext : DbContext
                 .HasName("PRIMARY")
                 .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
 
-            entity.ToTable("cart");
+            entity
+                .ToTable("cart")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.HasIndex(e => e.CustomerId, "fk_Cart_Customers1_idx");
 
@@ -86,7 +98,10 @@ public partial class MaindbContext : DbContext
         {
             entity.HasKey(e => e.CategoryId).HasName("PRIMARY");
 
-            entity.ToTable("categorys");
+            entity
+                .ToTable("categorys")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.Property(e => e.CategoryId)
                 .ValueGeneratedNever()
@@ -100,7 +115,10 @@ public partial class MaindbContext : DbContext
         {
             entity.HasKey(e => e.CustomerId).HasName("PRIMARY");
 
-            entity.ToTable("customers");
+            entity
+                .ToTable("customers")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.HasIndex(e => e.Email, "Email_UNIQUE").IsUnique();
 
@@ -125,11 +143,46 @@ public partial class MaindbContext : DbContext
             entity.Property(e => e.Phone).HasMaxLength(45);
         });
 
+        modelBuilder.Entity<District>(entity =>
+        {
+            entity.HasKey(e => e.DistrictId).HasName("PRIMARY");
+
+            entity.ToTable("districts");
+
+            entity.HasIndex(e => e.GovernorateId, "fk_Districts_Governorates1_idx");
+
+            entity.Property(e => e.DistrictId)
+                .ValueGeneratedNever()
+                .HasColumnName("DistrictID");
+            entity.Property(e => e.DistrictName).HasMaxLength(45);
+            entity.Property(e => e.GovernorateId).HasColumnName("GovernorateID");
+
+            entity.HasOne(d => d.Governorate).WithMany(p => p.Districts)
+                .HasForeignKey(d => d.GovernorateId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_Districts_Governorates1");
+        });
+
+        modelBuilder.Entity<Governorate>(entity =>
+        {
+            entity.HasKey(e => e.GovernorateId).HasName("PRIMARY");
+
+            entity.ToTable("governorates");
+
+            entity.Property(e => e.GovernorateId)
+                .ValueGeneratedNever()
+                .HasColumnName("GovernorateID");
+            entity.Property(e => e.GovernorateName).HasMaxLength(45);
+        });
+
         modelBuilder.Entity<MeasuresOfScale>(entity =>
         {
             entity.HasKey(e => e.MeasureOfScaleId).HasName("PRIMARY");
 
-            entity.ToTable("measures_of_scale");
+            entity
+                .ToTable("measures_of_scale")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.HasIndex(e => e.MeasureOfScale, "Measure_Of_Scale_UNIQUE").IsUnique();
 
@@ -145,16 +198,19 @@ public partial class MaindbContext : DbContext
         {
             entity.HasKey(e => e.OrderId).HasName("PRIMARY");
 
-            entity.ToTable("orders");
+            entity
+                .ToTable("orders")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.HasIndex(e => e.CustomerId, "fk_Orders_Customers1_idx");
+
+            entity.HasIndex(e => e.StreetId, "fk_orders_streats1_idx");
 
             entity.Property(e => e.OrderId)
                 .ValueGeneratedNever()
                 .HasColumnName("Order_ID");
-            entity.Property(e => e.Area).HasMaxLength(45);
             entity.Property(e => e.CustomerId).HasColumnName("Customer_ID");
-            entity.Property(e => e.House).HasMaxLength(45);
             entity.Property(e => e.OrderDate)
                 .HasColumnType("datetime")
                 .HasColumnName("Order_Date");
@@ -162,12 +218,17 @@ public partial class MaindbContext : DbContext
                 .HasMaxLength(45)
                 .HasColumnName("Order_Status");
             entity.Property(e => e.PayedByVisa).HasColumnName("Payed_By_Visa");
-            entity.Property(e => e.Streat).HasMaxLength(45);
+            entity.Property(e => e.StreetId).HasColumnName("StreetID");
 
             entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
                 .HasForeignKey(d => d.CustomerId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("fk_Orders_Customers1");
+
+            entity.HasOne(d => d.Street).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.StreetId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_orders_streats1");
         });
 
         modelBuilder.Entity<OrderdProduct>(entity =>
@@ -176,7 +237,10 @@ public partial class MaindbContext : DbContext
                 .HasName("PRIMARY")
                 .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
 
-            entity.ToTable("orderd_products");
+            entity
+                .ToTable("orderd_products")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.HasIndex(e => e.OrderId, "fk_Orderd_Products_Orders1_idx");
 
@@ -201,7 +265,10 @@ public partial class MaindbContext : DbContext
         {
             entity.HasKey(e => e.OrderId).HasName("PRIMARY");
 
-            entity.ToTable("payments");
+            entity
+                .ToTable("payments")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.Property(e => e.OrderId)
                 .ValueGeneratedNever()
@@ -230,7 +297,10 @@ public partial class MaindbContext : DbContext
         {
             entity.HasKey(e => e.ProductId).HasName("PRIMARY");
 
-            entity.ToTable("products");
+            entity
+                .ToTable("products")
+                .HasCharSet("utf8mb3")
+                .UseCollation("utf8mb3_general_ci");
 
             entity.HasIndex(e => e.BrandId, "fk_Products_Brands1_idx");
 
@@ -241,11 +311,11 @@ public partial class MaindbContext : DbContext
             entity.Property(e => e.ProductId).HasColumnName("Product_ID");
             entity.Property(e => e.BrandId).HasColumnName("Brand_ID");
             entity.Property(e => e.CategoryId).HasColumnName("Category_ID");
-            entity.Property(e => e.Description).HasColumnType("tinytext");
-            entity.Property(e => e.Image1).HasMaxLength(45);
-            entity.Property(e => e.Image2).HasMaxLength(45);
-            entity.Property(e => e.Image3).HasMaxLength(45);
-            entity.Property(e => e.Image4).HasMaxLength(45);
+            entity.Property(e => e.Description).HasColumnType("mediumtext");
+            entity.Property(e => e.Image1).HasMaxLength(200);
+            entity.Property(e => e.Image2).HasMaxLength(200);
+            entity.Property(e => e.Image3).HasMaxLength(200);
+            entity.Property(e => e.Image4).HasMaxLength(200);
             entity.Property(e => e.InstockQty).HasColumnName("Instock_QTY");
             entity.Property(e => e.MeasureOfScaleId).HasColumnName("Measure_Of_Scale_ID");
             entity.Property(e => e.NewOrUsed).HasColumnName("New_OR_Used");
@@ -271,6 +341,26 @@ public partial class MaindbContext : DbContext
                 .HasForeignKey(d => d.MeasureOfScaleId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("fk_Products_table11");
+        });
+
+        modelBuilder.Entity<Street>(entity =>
+        {
+            entity.HasKey(e => e.StreetId).HasName("PRIMARY");
+
+            entity.ToTable("streets");
+
+            entity.HasIndex(e => e.DistrictId, "fk_streets_Districts1_idx");
+
+            entity.Property(e => e.StreetId)
+                .ValueGeneratedNever()
+                .HasColumnName("StreetID");
+            entity.Property(e => e.DistrictId).HasColumnName("DistrictID");
+            entity.Property(e => e.StreetName).HasMaxLength(45);
+
+            entity.HasOne(d => d.District).WithMany(p => p.Streets)
+                .HasForeignKey(d => d.DistrictId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_streets_Districts1");
         });
 
         OnModelCreatingPartial(modelBuilder);
---
+++ Repository/Static.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure_Layer.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Data_Layer.Repository
+{
+    public class Static
+    {
+        public static Customer GetCustomer(HttpContext HttpContext, MaindbContext _context)
+        {
+            string myCookieValue = HttpContext.Request.Cookies["MyCookie"];
+            var person = _context.Customers.FirstOrDefault(x => x.Cookie == myCookieValue);
+            return person;
+        }
+    }
+}
